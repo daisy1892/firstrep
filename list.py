d8382e92ec0e197cd646a5c46f951a1f305e2a0b
daisy1892 <109541827+daisy1892@users.noreply.github.com>
2022-07-18 15:00:57 -0400

    Update list.py
    update second list

--- list.py
+++ list.py
@@ -2,6 +2,8 @@ from typing import Set
 
 anewlist=["zero","one" ,"two" ,"three" ,"four","five","six"]
 
+secondlist=["apple","banana" ,"orange"]
+
 
 
 
@@ -14,9 +16,9 @@ for x in anewlist:
 print(new_list)
 
 new_list=[x for x in anewlist]
+new_user
 print(new_list)
 
 new_list=[x if x!="three" else "orange" for x in anewlist]
 print(new_list)
-
-
+print(secondlist)
